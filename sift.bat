d658b9d25e6e8408aee070060ca9b2ee4bd3f84d
saltkid <ojisaltkid@gmail.com>
2024-01-19 01:27:14 +0800

    add error message if no repos found under dirs in dirs.txt

--- sift.bat
+++ sift.bat
@@ -10,10 +10,19 @@ if "%~1" neq "" (
 ) else (
     rem select git repos from the list of directories in dirs.txt
     for /f "usebackq delims=" %%i in (`type %~dp0dirs.txt`) do (
-        for /f "delims=" %%j in ('dir /b /ad /s "%%i\\*.git" ^| sed "s/\\.git$//"') do (
+        for /f "delims=" %%j in ('dir /b /ad /s "%%i\*.git" ^| sed "s/\\.git$//"') do (
 		echo %%j>>%~dp0temp_repos.txt
         )
     )
+ 
+    rem check if temp_repos.txt exists aka if found any git repos
+    if not exist "%~dp0temp_repos.txt" (
+        echo No local git repos found under:
+        type "%~dp0dirs.txt"
+        set "selected="
+        exit /b 0
+    )
+
     for /f "delims=" %%i in ('type %~dp0temp_repos.txt ^| fzf') do set "selected=%%~i"
 )
 
