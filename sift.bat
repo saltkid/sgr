b7e99500567613da3b8c54e145b545311541595c
saltkid <ojisaltkid@gmail.com>
2024-01-18 23:55:56 +0800

    add config file to put dirs that contain git repos
    can now add your own directories that contain git repos to fuzzy find. must be in the same directory as sift.bat

--- sift.bat
+++ sift.bat
@@ -3,14 +3,24 @@
 if "%~1" neq "" (
     set "selected=%~1"
 ) else (
-    rem select git repos
-    for /f "delims=" %%i in ('dir /b /ad /s "%USERPROFILE%\projects\*" ^| findstr /i "\\\.git$" ^| sed "s/\\.git$//" ^| fzf') do set "selected=%%~i"
+    rem select git repos from the list of directories in dirs.txt
+    for /f "usebackq delims=" %%i in (`type %~dp0dirs.txt`) do (
+        for /f "delims=" %%j in ('dir /b /ad /s "%%i\\*.git" ^| sed "s/\\.git$//"') do (
+		echo %%j>>%~dp0temp_repos.txt
+        )
+    )
+    for /f "delims=" %%i in ('type %~dp0temp_repos.txt ^| fzf') do set "selected=%%~i"
 )
 
 if "%selected%"=="" (
+    echo No directory selected.
+    set "selected="
+    del %~dp0temp_repos.txt
     exit /b 0
 )
 
 cd %selected%
+echo Now in: %selected%
 rem delete selected environment variable
 set "selected="
+del %~dp0temp_repos.txt
