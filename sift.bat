ae9152cca8e436d1fac258d4f0ceed0e036e5068
saltkid <ojisaltkid@gmail.com>
2024-01-19 18:06:37 +0800

    add 'list' command with optional specific line arg
    TODO: range arg

--- sift.bat
+++ sift.bat
@@ -5,38 +5,36 @@ if not exist "%~dp0dirs.txt" (
 )
 
 setlocal enabledelayedexpansion
-
 if not "%~1"=="" (
     if /i "%~1"=="add" (
         rem check if there's a path specified after "add"
         if not "%~2"=="" (
             rem check if the second argument is a valid path and contains at least one git repo
-            if exist "%2" (
+            if exist "%~2" (
                 echo set count=0
                 set "count=0"
-                for /f "delims=" %%j in ('dir /b /ad /s "%2\*.git" ^| sed "s/\\.git$//"') do (
+                for /f "delims=" %%j in ('dir /b /ad /s "%~2\*.git" ^| sed "s/\\.git$//"') do (
                     set /a count+=1
                 )
                 if !count! gtr 0 (
                     rem Validate if %~f2 already exists in %~dp0dirs.txt
                     for /f "usebackq delims=" %%k in ("%~dp0dirs.txt") do (
                         if "%%k"=="%~f2" (
-                            echo '%2' already exists in dirs.txt
+                            echo '%~f2' already exists in dirs.txt
                             exit /b 1
                         )
                     )
-                    echo.>>"%~dp0dirs.txt"
                     echo %~f2>>"%~dp0dirs.txt"
-                    echo added '%2'
+                    echo added "%~f2"
                     exit /b 0
 
                 ) else (
-                    echo No Git repositories found under %2
+                    echo No Git repositories found under "%~f2"
                     exit /b 1
                 )
 
             ) else (
-                echo %2 is not a valid path
+                echo "%~f2" is not a valid path
                 exit /b 1
             )
 
@@ -45,13 +43,45 @@ if not "%~1"=="" (
             exit /b 1
         )
     )
+    if /i "%~1"=="list" (
+        echo ---------------------------------------------------
+        echo idx ^| dirs
+        echo ---------------------------------------------------
+        set "line_num=1"
+
+        if not "%~2"=="" (
+            set "check_idx=%~2"
+            for /f "usebackq delims=" %%i in ("%~dp0dirs.txt") do (
+                if !line_num!==!check_idx! (
+                    set "pad_num=0!line_num!"
+                    echo  !pad_num:~-2! ^| "%%i"
+                    echo ---------------------------------------------------
+                    exit /b 0
+                )
+                set /a line_num+=1
+            )
+            echo line %~2 not found
+            echo ---------------------------------------------------
+            exit /b 1
+        )
+    )
+        for /f "usebackq delims=" %%i in ("%~dp0dirs.txt") do (
+            if not "%%i"=="" (
+                set "pad_num=0!line_num!"
+                echo  !pad_num:~-2! ^| "%%i"
+                set /a line_num+=1
+            )
+        )
+        echo ---------------------------------------------------
+        exit /b 0
+    )
 )
 endlocal
 
 rem select git repos from the list of directories in dirs.txt
 for /f "usebackq delims=" %%i in (`type %~dp0dirs.txt`) do (
     for /f "delims=" %%j in ('dir /b /ad /s "%%i\*.git" ^| sed "s/\\.git$//"') do (
-    	echo %%j>>%~dp0temp_repos.txt
+    	echo %%j>>"%~dp0temp_repos.txt"
     )
 )
 
@@ -68,7 +98,7 @@ for /f "delims=" %%i in ('type %~dp0temp_repos.txt ^| fzf') do set "selected=%%~
 if "%selected%"=="" (
     echo No directory selected.
     set "selected="
-    del %~dp0temp_repos.txt
+    del "%~dp0temp_repos.txt
     exit /b 0
 )
 
