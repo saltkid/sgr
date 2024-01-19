a8df4903812e6cca403f47e72753605ab2b26b37
saltkid <ojisaltkid@gmail.com>
2024-01-19 22:17:59 +0800

    added a range of two nums arg for 'list'

--- sift.bat
+++ sift.bat
@@ -25,16 +25,16 @@ if not "%~1"=="" (
                         )
                     )
                     echo %~f2>>"%~dp0dirs.txt"
-                    echo added "%~f2"
+                    echo added "%~2"
                     exit /b 0
 
                 ) else (
-                    echo No Git repositories found under "%~f2"
+                    echo No Git repositories found under "%~2"
                     exit /b 1
                 )
 
             ) else (
-                echo "%~f2" is not a valid path
+                echo "%~2" is not a valid path
                 exit /b 1
             )
 
@@ -48,23 +48,81 @@ if not "%~1"=="" (
         echo idx ^| dirs
         echo ---------------------------------------------------
         set "line_num=1"
+        set "list_arg=%~2"
 
         if not "%~2"=="" (
-            set "check_idx=%~2"
-            for /f "usebackq delims=" %%i in ("%~dp0dirs.txt") do (
-                if !line_num!==!check_idx! (
-                    set "pad_num=0!line_num!"
-                    echo  !pad_num:~-2! ^| "%%i"
-                    echo ---------------------------------------------------
-                    exit /b 0
+            set /a "hyphen_position=0"
+            set /a "hyphen_count=0"
+            rem identify the position of the hyphen
+            for /l %%i in (0,1,100) do (
+                set "char=!list_arg:~%%i,1!"
+                if "!char!"=="-" (
+                    if %%i==0 (
+                        echo negative numbers are not allowed
+                        exit /b 1
+                    )
+                    set /a hyphen_count+=1
+                    set /a hyphen_position=%%i
                 )
-                set /a line_num+=1
             )
-            echo line %~2 not found
-            echo ---------------------------------------------------
-            exit /b 1
+
+            if !hyphen_count!==1 (
+                rem split %2 into two parts
+                for /f "tokens=1,* delims=-" %%a in ("!list_arg!") do (
+                    set "range_start=%%a"
+                    set "range_end=%%b"
+                )
+
+                rem check if each part is a number
+                if 1!range_start! neq +1!range_start! (
+                    echo !range_start! is not a valid starting range
+                    exit /b 1
+                )
+                if 1!range_end! neq +1!range_end! (
+                    echo !range_end! is not a valid ending range
+                    exit /b 1
+                )
+                rem check if starting range is smaller than ending range
+                if !range_start! gtr !range_end! (
+                    echo starting range '!range_start!' must be smaller than ending range '!range_end!'
+                    exit /b 1
+                )
+                rem valid range, get dirs
+                for /f "usebackq delims=" %%i in ("%~dp0dirs.txt") do (
+                    if !line_num! geq !range_start! if !line_num! leq !range_end! (
+                        set "pad_num=0!line_num!"
+                        echo  !pad_num:~-2! ^| "%%i"
+                    )
+                    set /a line_num+=1
+                )
+                echo ---------------------------------------------------
+                exit /b 0
+            )
+
+            if !hyphen_count!==0 (
+                rem check if a number
+                if 1%2 neq +1%2 (
+                    echo %~2 is not a valid line number
+                    exit /b 1
+                )
+
+                set "check_idx=%~2"
+                for /f "usebackq delims=" %%i in ("%~dp0dirs.txt") do (
+                    if !line_num!==!check_idx! (
+                        set "pad_num=0!line_num!"
+                        echo  !pad_num:~-2! ^| "%%i"
+                        echo ---------------------------------------------------
+                        exit /b 0
+                    )
+                    set /a line_num+=1
+                )
+                echo line %~2 not found
+                echo ---------------------------------------------------
+                exit /b 1
+            )
+
         )
-    )
+
         for /f "usebackq delims=" %%i in ("%~dp0dirs.txt") do (
             if not "%%i"=="" (
                 set "pad_num=0!line_num!"
