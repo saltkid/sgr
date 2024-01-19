6190df88b9e2d32fedf7c2a5592be952388aaac5
saltkid <ojisaltkid@gmail.com>
2024-01-19 12:28:36 +0800

    'add' command to add a path to dirs.txt with validations
    check if exists, check if already included in dirs.txt

--- sift.bat
+++ sift.bat
@@ -4,27 +4,66 @@ if not exist "%~dp0dirs.txt" (
     echo %userprofile%\projects>"%~dp0dirs.txt"
 )
 
+setlocal enabledelayedexpansion
 
-if "%~1" neq "" (
-    set "selected=%~1"
-) else (
-    rem select git repos from the list of directories in dirs.txt
-    for /f "usebackq delims=" %%i in (`type %~dp0dirs.txt`) do (
-        for /f "delims=" %%j in ('dir /b /ad /s "%%i\*.git" ^| sed "s/\\.git$//"') do (
-		echo %%j>>%~dp0temp_repos.txt
+if not "%~1"=="" (
+    if /i "%~1"=="add" (
+        rem check if there's a path specified after "add"
+        if not "%~2"=="" (
+            rem check if the second argument is a valid path and contains at least one git repo
+            if exist "%2" (
+                echo set count=0
+                set "count=0"
+                for /f "delims=" %%j in ('dir /b /ad /s "%2\*.git" ^| sed "s/\\.git$//"') do (
+                    set /a count+=1
+                )
+                if !count! gtr 0 (
+                    rem Validate if %~f2 already exists in %~dp0dirs.txt
+                    for /f "usebackq delims=" %%k in ("%~dp0dirs.txt") do (
+                        if "%%k"=="%~f2" (
+                            echo '%2' already exists in dirs.txt
+                            exit /b 1
+                        )
+                    )
+                    echo.>>"%~dp0dirs.txt"
+                    echo %~f2>>"%~dp0dirs.txt"
+                    echo added '%2'
+                    exit /b 0
+
+                ) else (
+                    echo No Git repositories found under %2
+                    exit /b 1
+                )
+
+            ) else (
+                echo %2 is not a valid path
+                exit /b 1
+            )
+
+        ) else (
+            echo No path specified for "sift add"
+            exit /b 1
         )
     )
- 
-    rem check if temp_repos.txt exists aka if found any git repos
-    if not exist "%~dp0temp_repos.txt" (
-        echo No local git repos found under:
-        type "%~dp0dirs.txt"
-        set "selected="
-        exit /b 0
-    )
-
-    for /f "delims=" %%i in ('type %~dp0temp_repos.txt ^| fzf') do set "selected=%%~i"
 )
+endlocal
+
+rem select git repos from the list of directories in dirs.txt
+for /f "usebackq delims=" %%i in (`type %~dp0dirs.txt`) do (
+    for /f "delims=" %%j in ('dir /b /ad /s "%%i\*.git" ^| sed "s/\\.git$//"') do (
+    	echo %%j>>%~dp0temp_repos.txt
+    )
+)
+
+rem check if temp_repos.txt exists aka if found any git repos
+if not exist "%~dp0temp_repos.txt" (
+    echo No local git repos found under:
+    type "%~dp0dirs.txt"
+    set "selected="
+    exit /b 0
+)
+
+for /f "delims=" %%i in ('type %~dp0temp_repos.txt ^| fzf') do set "selected=%%~i"
 
 if "%selected%"=="" (
     echo No directory selected.
