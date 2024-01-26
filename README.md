7c14487a7f0778f974872d0bdec33c218458361a
saltkid <ojisaltkid@gmail.com>
2024-01-27 00:21:29 +0800

    add powershell script (better version)
    better version because it has real-time data piping unlike batch which I needed to find a workaround (temp files)

--- README.md
+++ README.md
@@ -3,17 +3,18 @@
 - *selected directories* are defined in `dirs.txt` in the same directory as **sift**
 - *selected directories* **contain** git repos **AND** can be git repos themselves
 
-## Dependencies
-- [fzf](https://github.com/junegunn/fzf) - for the fuzzy findng
-- [sed](https://github.com/mbuilov/sed-windows) - for truncating `\.git\` from the found git repos
+## Dependencies (must be installed and added to your PATH)
+- [fzf](https://github.com/junegunn/fzf)
+- [sed](https://github.com/mbuilov/sed-windows)
 
 *You can install both with [chocolatey](https://chocolatey.org/)*
 ```
 choco install fzf sed
 ```
 
-## Usage
-1. Clone the repo **OR** download `sift.bat`
+## Getting Started
+### Installation
+1. Download `sift.bat`
 2. ***Optionally*** add `sift.bat` to your PATH
 3. ***Optionally*** create `dirs.txt` in the same directory as `sift.bat` and put directories containing git repos.
     - if there's no `dirs.txt` in the same directory as `sift.bat`, it will create one containing only one line:
@@ -30,10 +31,26 @@ choco install fzf sed
     %other_env_vars%\are\also\okay
     %userprofile%\path\to\single\git\repo\is\okay
     ```
-
-4. Open a terminal and run `path/to/sift.bat`
+### Basic Usage
+1. Open a terminal and run `path/to/sift.bat`
     - if you have `sift.bat` in your PATH, you can just run `sift`
-5. Now, it'll open up the usual [fzf](https://github.com/junegunn/fzf) interface. Select a repo and it'll `cd` your current terminal to that path
+2. Now, it'll open up the usual [fzf](https://github.com/junegunn/fzf) interface. Select a repo and it'll `cd` your current terminal to that path
+
+### Commands
+**sift** has optional commands that handles modifying `dirs.txt`
+1. `add`
+    - **args**: `path/to/dir`
+    - adds `path/to/dir` to `dirs.txt` which will be searched for git repos when **sift** is run
+2. `remove`
+    - **args**: `path/to/dir`, `<int>`, `<int_x>-<int_y>`
+    - removes `path/to/dir` from `dirs.txt` if it's in `dirs.txt`
+    - if `<int>` is provided, it will remove the dir at line `<int>` in `dirs.txt`
+    - if `<int_x>-<int_y>` is provided, it will remove the dirs between lines `<x>` and `<y>`
+3. `list`
+    - **args**: none, `<int>`, `<int_x>-<int_y>`
+    - lists `dirs.txt` if no args are given
+    - if `<int>` is provided, it will list the dir at line `<int>` in `dirs.txt`
+    - if `<int_x>-<int_y>` is provided, it will list the dirs between lines `<x>` and `<y>`
 
 ## Extra
 ### 1. Calling sift through a shortcut in Windows Terminal
@@ -61,12 +78,13 @@ Below will open a new tab with the [fzf](https://github.com/junegunn/fzf) prompt
     }
 }
 ```
+
 *Notes:* 
-- both of these snippets assume you have `sift.bat` in your PATH. If not, use `"input": "\"path\\to\\sift.bat\"\u000D"` instead
+- these extras assume you have `sift.bat` in your PATH. If not, use `"input": "\"path\\to\\sift.bat\"\u000D"` instead
 - `ctrl+shift+f` is by default bound to the action of *opening the search dialog box* in Windows Terminal. If you want `ctrl+shift+f` for running **sift** in *new tab*, remap the `"find"` command to something else like `ctrl+alt+f`
 - Windows uses backslashes so those need to be escaped here. Example:
 ```
-"commandline" : "cmd.exe /K \"C:\\Users\\saltkid\\tools\\sift.bat\""
+"commandline" : "cmd.exe /K \"C:\\Users\\username\\tools\\sift.bat\""
 ```
 The two escaped quotation marks are there incase your path has spaces in it.
 
---
+++ sift2.ps1
@@ -0,0 +1,188 @@
+$version = "0.1.0"
+
+function Usage
+{
+    Write-Host "Usage: sift <optional-command> <optional-args>"
+    Write-Host "Examples:"
+    Write-Host "  sift"
+    Write-Host "  sift add path\to\dir"
+    Write-Host "  sift remove path\to\dir"
+    Write-Host "  sift list"
+}
+
+function ListPaths
+{
+    param (
+        [Parameter(Mandatory = $true)]
+        [int] $start,
+        [Parameter(Mandatory = $true)]
+        [int] $end,
+        [Parameter(Mandatory = $true)]
+        [array] $configContents
+    )
+    Write-Host "---------------------------------------------------------"
+    Write-Host "| idx | paths"
+    Write-Host "---------------------------------------------------------"
+    for ($i = $start; $i -lt $end; $i++)
+    {
+        Write-Host "|" "$($i + 1)".PadLeft(3)  "| $($configContents[$i])"
+    }
+    Write-Host "---------------------------------------------------------"
+}
+
+# check if dirs.txt exists first in script path 
+if (!(Test-Path "$PSScriptRoot\dirs.txt"))
+{
+    # default dir
+    "$env:userprofile\projects" | Out-File -FilePath "$PSScriptRoot\dirs.txt"
+}
+
+# main functionality: using fzf to find git repos
+if ($args.Count -lt 1)
+{
+    $selectedRepo = Get-Content "$PSScriptRoot\dirs.txt" | ForEach-Object {
+        # force because .git is a hidden directory
+        Get-ChildItem -Path $_ -Filter "*.git" -Recurse -Directory -Force |
+            ForEach-Object {
+                # remove the .git from the path before piping it into fzf
+                $_.FullName -replace '\\\.git$'
+            }
+        } | fzf
+
+    if (-not [string]::IsNullOrWhiteSpace($selectedRepo))
+    {
+        Set-Location $selectedRepo
+        Write-Host "Selected: $selectedRepo"
+    } else
+    {
+        Write-Host "No repository selected."
+    }    exit 0
+}
+
+# put here the valid commands
+$command = $args[0]
+if (!($args[0] -eq "list") -and !($args[0] -eq "add") -and !($args[0] -eq "remove") -and !($args[0] -eq "help") -and !($args[0] -eq "version"))
+{
+    Write-Host "Invalid command: $command" -ForegroundColor Red
+    Usage
+    exit 1
+}
+
+# put here commands that allow no values
+if (($args.Count -lt 2) -and !($command -eq "list") -and !($command -eq "help") -and !($command -eq "version"))
+{
+    Write-Host "Missing argument for command: $command" -ForegroundColor Red
+    Usage
+    exit 1
+}
+
+# extra args after first two will be unused, but don't stop the execution
+if ($args.Count -gt 2)
+{
+    $unused = $args[2..($args.Count - 1)]
+    Write-Host "'$unused' will be unused" -ForegroundColor Yellow
+}
+
+if ($args[0] -eq "add")
+{
+    Write-Host "Adding $($args[1])..."
+    if (!(Test-Path $args[1]))
+    {
+        Write-Host "$($args[1]) does not exist" -ForegroundColor Red
+        exit 1
+    }
+    $path = Resolve-Path $args[1]
+
+    $configContents = Get-Content "$PSScriptRoot\dirs.txt"
+    if ($configContents -contains $path)
+    {
+        Write-Host "$path already exists in $PSScriptRoot\dirs.txt" -ForegroundColor Red
+        exit 1
+    } else
+    {
+        $path | Add-Content "$PSScriptRoot\dirs.txt"
+        Write-Host "$path added to $PSScriptRoot\dirs.txt"
+        exit 0
+    }
+
+} elseif ($args[0] -eq "remove")
+{
+    Write-Host "Removing $($args[1])..."
+    $configContents = Get-Content "$PSScriptRoot\dirs.txt"
+    if (!(Test-Path $args[1]))
+    {
+        Write-Host "$($args[1]) does not exist" -ForegroundColor Red
+        exit 1
+    }
+
+    $path = Resolve-Path $args[1]
+    if ($configContents -contains $path)
+    {
+        $configContents = $configContents | Where-Object { $_ -ne $path }
+        $configContents | Set-Content "$PSScriptRoot\dirs.txt"
+        Write-Host "$path removed from $PSScriptRoot\dirs.txt"
+        exit 0
+    } else
+    {
+        Write-Host "Nothing to remove" -ForegroundColor Red
+        Write-Host "$path does not exist in $PSScriptRoot\dirs.txt" -ForegroundColor Red
+        exit 1
+    }
+
+} elseif ($args[0] -eq "list")
+{
+    $listArg = $args[1]
+
+    $configContents = Get-Content "$PSScriptRoot\dirs.txt"
+
+    if (($listArg -eq "all") -or (-not $listArg))
+    {
+        ListPaths -start 0 -end $configContents.Count -configContents $configContents
+
+    } elseif ($listArg -match "^\d+$")
+    {
+        $index = [int]$listArg - 1
+        if ($index -ge 0 -and $index -lt $configContents.Count)
+        {
+            ListPaths -start $index -end $($index+1) -configContents $configContents
+
+        } elseif ($index -lt 0)
+        {
+            Write-Host "Index $index is out of range" -ForegroundColor Red
+            exit 1
+        } elseif ($index -ge $configContents.Count)
+        {
+            Write-Host "Index $index is out of range" -ForegroundColor Red
+            exit 1
+        } else
+        {
+            Write-Host "Unexpected error: index = $index" -ForegroundColor Red
+            exit 1
+        }
+    } elseif ($listArg -match "^\d+-\d+$")
+    {
+        # assert that x should be less than y
+        $x, $y = $listArg -split "-"
+        $x, $y = [int]$x, [int]$y
+        $x = $x - 1
+        $y = $y - 1
+        if ($x -ge $y)
+        {
+            Write-Host "Invalid range: $listArg; starting range '$x' should be less than ending range '$y'" -ForegroundColor Red
+            exit 1
+        } elseif ($x -ge $configContents.Count -or $x -lt 0 -or $y -ge $configContents.Count -or $y -lt 0)
+        {
+            Write-Host "Index $x and/or $y is out of range" -ForegroundColor Red
+            exit 1
+        }
+        
+        ListPaths -start $x -end $($y + 1) -configContents $configContents
+    }
+} elseif ($args[0] -eq "help")
+{
+    Usage
+} elseif ($args[0] -eq "version")
+{
+    Write-Host "sift version $version"
+}
+exit 0
