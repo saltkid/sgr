985a0ec78c48bb65ead7333fe45d1b5f6ef29150
saltkid <ojisaltkid@gmail.com>
2024-01-27 18:10:52 +0800

    remove by index and index range

--- sift.ps1
+++ sift.ps1
@@ -13,15 +13,16 @@ function Usage
 function ListPaths
 {
     param (
+        [string] $header = "$PSScriptRoot\dirs.txt", 
+        [Parameter(Mandatory = $true)]
+        [array] $configContents,
         [Parameter(Mandatory = $true)]
         [int] $start,
         [Parameter(Mandatory = $true)]
-        [int] $end,
-        [Parameter(Mandatory = $true)]
-        [array] $configContents
+        [int] $end
     )
     Write-Host "---------------------------------------------------------"
-    Write-Host "| idx | paths"
+    Write-Host "| $header"
     Write-Host "---------------------------------------------------------"
     for ($i = $start; $i -lt $end; $i++)
     {
@@ -55,7 +56,8 @@ if ($args.Count -lt 1)
     } else
     {
         Write-Host "No repository selected."
-    }    exit 0
+    }
+    exit 0
 }
 
 # put here the valid commands
@@ -100,32 +102,86 @@ if ($args[0] -eq "add")
     } else
     {
         $path | Add-Content "$PSScriptRoot\dirs.txt"
-        Write-Host "$path added to $PSScriptRoot\dirs.txt"
+        ListPaths -header "updated $PSScriptRoot\dirs.txt" -start 0 -end $configContents.Count -configContents $configContents
+        Write-Host "|" "$($configContents.Count + 1)".PadLeft(3)  "| Added: $path"
+        Write-Host "---------------------------------------------------------"
         exit 0
     }
 
 } elseif ($args[0] -eq "remove")
 {
-    Write-Host "Removing $($args[1])..."
-    $configContents = Get-Content "$PSScriptRoot\dirs.txt"
-    if (!(Test-Path $args[1]))
-    {
-        Write-Host "$($args[1]) does not exist" -ForegroundColor Red
-        exit 1
-    }
+    $removeArg = $args[1]
 
-    $path = Resolve-Path $args[1]
-    if ($configContents -contains $path)
+    $configContents = Get-Content "$PSScriptRoot\dirs.txt"
+
+    if ($removeArg -eq "all")
     {
-        $configContents = $configContents | Where-Object { $_ -ne $path }
-        $configContents | Set-Content "$PSScriptRoot\dirs.txt"
-        Write-Host "$path removed from $PSScriptRoot\dirs.txt"
+        # delete the file
+        Remove-Item "$PSScriptRoot\dirs.txt"
+        "$env:userprofile\projects" | Out-File -FilePath "$PSScriptRoot\dirs.txt"
+        $updatedContents = Get-Content "$PSScriptRoot\dirs.txt"
+        ListPaths -header "reset $PSScriptRoot\dirs.txt" -configContents $updatedContents -start 0 -end $updatedContents.Count
+        Write-Host "| All paths except $env:userprofile\projects removed"
+        Write-Host "---------------------------------------------------------"
         exit 0
+
+    } elseif ($removeArg -match '^[0-9]+$')
+    {
+        $index = [int]$removeArg - 1
+        if ($index -ge 0 -and $index -lt $configContents.Count)
+        {
+            $path = $configContents[$index]
+            $updatedContents = $configContents | Where-Object { $_ -ne $path }
+            $updatedContents | Set-Content "$PSScriptRoot\dirs.txt"
+            ListPaths -header "updated $PSScriptRoot\dirs.txt" -start 0 -end $updatedContents.Count -configContents $updatedContents
+            Write-Host "|" "$($index + 1)".PadLeft(3)  "| Removed: $path"
+            Write-Host "---------------------------------------------------------"
+            exit 0
+        } else
+        {
+            Write-Host "Invalid index: $($index + 1)" -ForegroundColor Red
+            exit 1
+        }
+    } elseif ($removeArg -match '^[0-9]+-[0-9]+$')
+    {
+        $range = $removeArg -split '-'
+        $start = [int]$range[0] - 1
+        $end = [int]$range[1] - 1
+        if ($start -ge 0 -and $end -ge 0 -and $start -lt $configContents.Count -and $end -lt $configContents.Count)
+        {
+            $updatedContents = $configContents | Where-Object { $_ -notin $configContents[$start..$end] }
+            $updatedContents | Set-Content "$PSScriptRoot\dirs.txt"
+            ListPaths -header "updated $PSScriptRoot\dirs.txt" -start 0 -end $updatedContents.Count -configContents $updatedContents
+            ListPaths -header "removed paths" -start $start -end $($end+1) -configContents $configContents
+            exit 0
+        } else
+        {
+            Write-Host "Invalid range: $range" -ForegroundColor Red
+            exit 1
+        }
     } else
     {
-        Write-Host "Nothing to remove" -ForegroundColor Red
-        Write-Host "$path does not exist in $PSScriptRoot\dirs.txt" -ForegroundColor Red
-        exit 1
+        if (!(Test-Path $args[1]))
+        {
+            Write-Host "$($args[1]) does not exist" -ForegroundColor Red
+            exit 1
+        }
+
+        $path = Resolve-Path $args[1]
+        if ($configContents -contains $path)
+        {
+            $updatedContents = $configContents | Where-Object { $_ -ne $path }
+            $updatedContents | Set-Content "$PSScriptRoot\dirs.txt"
+            ListPaths -header "updated $PSScriptRoot\dirs.txt" -start 0 -end $updatedContents.Count -configContents $updatedContents
+            Write-Host "|" "$($configContents.IndexOf($path) + 1)".PadLeft(3)  "| Removed: $path"
+            Write-Host "---------------------------------------------------------"
+            exit 0
+        } else
+        {
+            Write-Host "Nothing to remove" -ForegroundColor Red
+            Write-Host "$path does not exist in $PSScriptRoot\dirs.txt" -ForegroundColor Red
+            exit 1
+        }
     }
 
 } elseif ($args[0] -eq "list")
@@ -184,4 +240,3 @@ if ($args[0] -eq "add")
 {
     Write-Host "sift version $version"
 }
-exit 0
