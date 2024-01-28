b7ddbc5b1310b1535fcd4a7e079194326587a8b3
saltkid <ojisaltkid@gmail.com>
2024-01-29 01:08:15 +0800

    initial impl: add command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::fs::OpenOptions;
+use std::io::{BufRead, BufReader, Write};
+use std::path::Path;
+
 fn main() {
     let raw_args: Vec<String> = std::env::args().collect();
 
@@ -12,35 +16,112 @@ fn main() {
 
     let command = &raw_args[1];
     let args = &raw_args[2..];
+    if args.is_empty() {
+        eprintln!("no args given for '{command}'");
+        return;
+    }
 
-    let result = match command.as_str() {
-        "add" => add(args),
-        "remove" => remove(args),
-        "list" => list(args),
+    if args.len() > 1 {
+        eprintln!("args '{:?}' will be unused", args[1..].to_vec());
+    }
+
+    let arg = &args[0].to_string();
+    let res = match command.as_str() {
+        "add" => add(arg),
+        "remove" => remove(arg),
+        "list" => list(arg),
         _ => Err("unknown command".to_string()),
     };
 
-    match result {
+    match res {
         Ok(_) => {}
         Err(e) => println!("{}", e),
     }
 }
 
 fn run() -> Result<(), String> {
-    println!("ran run");
-    return Ok(());
-}
-fn add(args: &[String]) -> Result<(), String> {
-    println!("ran add with {:?}", args);
-    return Ok(());
+    let file = OpenOptions::new()
+        .read(true)
+        .open("dirs.txt")
+        .map_err(|e| format!("Failed to open file \"dirs.txt\": {}", e))?;
+
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        match line {
+            Ok(line) => {
+                println!("{}", line);
+            }
+            Err(e) => {
+                return Err(format!("Failed to read line: {}", e));
+            }
+        }
+    }
+
+    Ok(())
 }
 
-fn remove(args: &[String]) -> Result<(), String> {
+fn add(dir: &String) -> Result<(), String> {
+    let abs_path = Path::new(&dir)
+        .canonicalize()
+        .map_err(|e| format!("Failed to canonicalize path: {}", e))?;
+
+    if !abs_path.is_dir() {
+        return Err(format!("\"{}\" is not a directory", abs_path.display()));
+    }
+
+    // TODO: check if abs_path contains any .git subdir; do walkdir
+
+    let mut file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .append(true)
+        .open("dirs.txt")
+        .map_err(|e| format!("Failed to open file \"dirs.txt\": {}", e))?;
+
+    let reader = BufReader::new(&file);
+    for (i, line) in reader.lines().enumerate() {
+        match line {
+            Ok(line) => {
+                if line == abs_path.display().to_string() {
+                    return Err(format!(
+                        "\"{}\" already exists in dirs.txt on line {}",
+                        abs_path.display(),
+                        i + 1
+                    ));
+                }
+            }
+            Err(e) => {
+                return Err(format!(
+                    "Failed to read \"dirs.txt\" at line {}: {}",
+                    i + 1,
+                    e
+                ));
+            }
+        }
+    }
+
+    let to_write = abs_path
+        .display()
+        .to_string()
+        .strip_prefix(r#"\\?\"#)
+        .unwrap_or(&abs_path.display().to_string())
+        .to_string();
+
+    if let Err(e) = writeln!(file, "{}", to_write) {
+        return Err(format!("Failed to write to file: {}", e));
+    }
+
+    println!("{} added to dirs.txt", to_write);
+    Ok(())
+}
+
+fn remove(args: &String) -> Result<(), String> {
     println!("ran remove with {:?}", args);
     return Ok(());
 }
 
-fn list(args: &[String]) -> Result<(), String> {
+fn list(args: &String) -> Result<(), String> {
     println!("ran list with {:?}", args);
     return Ok(());
 }
