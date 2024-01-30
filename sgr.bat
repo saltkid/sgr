298e3d8778e43dec91d2c83a4836feb259b70d60
saltkid <ojisaltkid@gmail.com>
2024-01-30 15:43:27 +0800

    accomodate ansi color codes in .bat and .ps1 files

--- sgr.bat
+++ sgr.bat
@@ -1,9 +1,14 @@
 @echo off
 
+rem to properly escape ansi color codes
+for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do set ESC=%%b
+
+rem create dirs.txt if doesn't exist yet
 if not exist "%~dp0dirs.txt" (
     echo %userprofile%\projects>"%~dp0dirs.txt"
 )
 
+rem properly pass args to sgr
 if "%~1"=="" (
     goto select_repos
 )
@@ -13,15 +18,24 @@ if not "%~1"=="" (
 )
 
 :select_repos
-for /f %%i in ('cargo run') do (
-    set "selected_path=%%i"
-)
-if "%selected_path%"=="" (
-    goto end
-)
+    rem select the entire message sent by sgr
+    for /f "tokens=*" %%i in ('cargo run') do (
+        set "selected_path=%%i"
+    )
 
-cd /d "%selected_path%" 
-echo Changed Directory to "%selected_path%"
+    rem select only the header (should be either [WARN] or a path)
+    for /f "tokens=1 delims= " %%a in ("%selected_path%") do (
+        if %%a==%ESC%[33m[WARN]%ESC%[0m (
+            rem echo warning message
+            echo %selected_path%
+            goto end
+        )
+    )
 
+    cd /d "%selected_path%" 
+    echo Changed Directory to "%selected_path%"
+
+rem remove env vars
 :end
-set "selected_path="
+    set "selected_path="
+    set "ESC="
